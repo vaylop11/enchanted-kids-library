8d2c20f0958c0bde396325b4527a28baeba90c46
vaylop11 <157020125+vaylop11@users.noreply.github.com>
2025-09-18 16:37:26 +0100

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://nknrkkzegbrkqtutmafo.supabase.co";  // 🔗 ضع رابط مشروعك
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbnJra3plZ2Jya3F0dXRtYWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNTQzNzUsImV4cCI6MjA1NzgzMDM3NX0.SggdJAWbi2cSM3gGP08NgCJLO2txiju9BivxU7Pznjk";   // 🔑 ضع الـ anon key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
